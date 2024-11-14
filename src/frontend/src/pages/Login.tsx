4262202adcd87912ae40a2dcbd54e8c36a7ea617
Luke Cunningham <psylc12@exmail.nottingham.ac.uk>
2024-11-14 18:53:06 +0000

    change login page so that it is user specific

--- src/frontend/src/pages/Login.tsx
+++ src/frontend/src/pages/Login.tsx
@@ -1,21 +1,70 @@
 import '../styles/styles.css'
-import Header from '../components/Header'
-import Home from './Home'
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
+import Header from '../components/Header'
 
-const Login = () => {
+function validatelogin (username : string, password: string) {
+    let validCredentials : string [][] = [['user1', 'password1'],['user2', 'password2']];
+
+    if ((username === validCredentials[0][0] && password === validCredentials[0][1])
+        || (username === validCredentials[1][0] && password === validCredentials[1][1])) {
+        return true
+    }
+    return false
+}
+
+
+function Login () {
+    const [Username, setUsername] = React.useState('');
+    const [Password, setPassword] = React.useState('');
+    let responseMsg = document.getElementById('loginResponse') as HTMLInputElement;
+    const navigation = useNavigate();
+
+    function onSubmit (){
+        if (validatelogin(Username, Password)){
+            if (responseMsg != null){
+                responseMsg.innerText = "";
+            }
+
+            navigation(`/Recapped/${Username}`);
+
+        }else{
+            let usernameValue =  document.getElementById('usernameBox') as HTMLInputElement;
+            let passwordValue =  document.getElementById('passwordBox') as HTMLInputElement;
+
+            if (usernameValue != null){
+                usernameValue.value = '';
+            }
+            if (passwordValue != null){
+                passwordValue.value = '';
+            }
+
+            if (responseMsg != null){
+                responseMsg.innerText = "Invalid Credentials";
+            }
+        }
+    }
+
     return (
+        <div className='LoginPage'>
+            <Header />
        <div className='login'>
-        <h1>Login</h1>
-        <form>
-            <label>Username</label>
-            <input type="text" required></input>
-            <label>Password</label>
-            <input type="password" required></input>
-            <button type="submit">Login</button>
-        </form>
-       </div> 
-    )
+            <h1>Login</h1>
+            <form>
+                <label>Username</label>
+                <input id = "usernameBox" type="text" required
+                onChange = {(e) => setUsername(e.target.value)}></input>
+
+                <label>Password</label>
+                <input id = "passwordBox" type="password" required
+                onChange = {(e) => setPassword(e.target.value)}></input>
+
+                <button id = "loginBtn" type="button" onClick={onSubmit}>Login</button>
+                <strong id = "loginResponse"></strong>
+            </form>
+           </div>
+        </div>
+    );
 
 }
 
---
+++ src/frontend/src/pages/Recapped.tsx
@@ -0,0 +1,14 @@
+import '../styles/styles.css'
+import React from 'react';
+
+function Recapped () {
+    return (
+       <div className='Recapped'>
+           <strong> Hello Recapped </strong>
+       </div> 
+    )
+
+}
+
+
+export default Recapped;
