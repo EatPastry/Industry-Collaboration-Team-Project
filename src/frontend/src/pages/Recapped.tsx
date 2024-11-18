c086bbff39814548ff1da08cddbc28a861b1eead
psylc12 <psylc12@nottingham.ac.uk>
2024-11-18 14:47:35 +0000

    Include DataString in Recapped.jsx

--- src/frontend/src/pages/Recapped.tsx
+++ src/frontend/src/pages/Recapped.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../styles/styles.css'
+import DataString from '../components/DataString'
 
 
 function Recapped () {
     return (
        <div className='Recapped'>
+            <DataString />
            <strong> Hello Recapped </strong>
            <h1 id = "myElement">Hello World</h1>
-    
        </div> 
     )
 
