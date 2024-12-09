b56739462d348e446edffba50fc77326ae0b8f30
Adrian <psyan13@nottingham.ac.uk>
2024-12-09 16:23:25 +0000

    add description to use case diagram

---
+++ src/frontend/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import '../styles/styles.css'
+import Header from '../components/Header'
+import React from 'react';
+
+function Home(){
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default Home;
+
