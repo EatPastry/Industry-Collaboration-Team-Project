dc6a41ddc85db492882381263c983b1ff520b8d9
psylc12 <psylc12@nottingham.ac.uk>
2025-02-05 21:07:18 +0000

    add google sign in button

--- src/frontend/src/pages/SignUp.tsx
+++ src/frontend/src/pages/SignUp.tsx
@@ -3,6 +3,7 @@ import React, {useRef, useState} from 'react';
 import Header from '../components/Header'
 import "../controller/Authentication";
 import {useNavigate} from 'react-router-dom';
+import GoogleButton from "../components/GoogleButton";
 
 /**
  * Creates the elements for the SignUp page
@@ -40,52 +41,54 @@ function SignUp () {
 
     return (
         <div className='signUpPage'>
-            <Header />
-                <div className='login'>
-                    <div id="title">Sign Up</div>
-                    <form ref={formReference}>
-                        {
-                            pageNum === 1? (
-                                <>
-                                    <label>First Name</label>
-                                    <input type="text" id = "firstName" value = {firstName}
-                                           onChange={(e) => setFirstName(e.target.value.trim())}
-                                           required></input>
+            <Header/>
+            <div className='login'>
+                <div id="title">Sign Up</div>
+                <form ref={formReference}>
+                    {
+                        pageNum === 1 ? (
+                            <>
+                                <label>First Name</label>
+                                <input type="text" id="firstName" value={firstName}
+                                       onChange={(e) => setFirstName(e.target.value.trim())}
+                                       required></input>
 
-                                    <label>Last Name</label>
-                                    <input type="text" id = "lastName" placeholder="(optional)" value = {lastName}
-                                           onChange={(e) => setLastName(e.target.value.trim())}
-                                           required></input>
+                                <label>Last Name</label>
+                                <input type="text" id="lastName" placeholder="(optional)" value={lastName}
+                                       onChange={(e) => setLastName(e.target.value.trim())}
+                                       required></input>
 
-                                    <div id="buttonWrapper">
-                                        <button id="switchBtn" type="button" onClick={() => navigation('/')}>Log In</button>
-                                        <button id="actionBtn" type="button" onClick={switchPageNum}>Next</button>
-                                    </div>
-                                </>
-                            ) : (
-                                <>
-                                    <label>Username</label>
-                                    <input type="text" value = {userName}
-                                           onChange={(e) => setUserName(e.target.value.trim())}
-                                           required
-                                    ></input>
+                                <div id="buttonWrapper">
+                                    <button id="switchBtn" type="button" onClick={() => navigation('/')}>Log In</button>
+                                    <button id="actionBtn" type="button" onClick={switchPageNum}>Next</button>
+                                </div>
+                            </>
+                        ) : (
+                            <>
+                                <label>Username</label>
+                                <input type="text" value={userName}
+                                       onChange={(e) => setUserName(e.target.value.trim())}
+                                       required
+                                ></input>
 
-                                    <label>Password</label>
-                                    <input type="password" value = {password}
-                                           onChange={(e) => setPassword(e.target.value.trim())}
-                                           required></input>
+                                <label>Password</label>
+                                <input type="password" value={password}
+                                       onChange={(e) => setPassword(e.target.value.trim())}
+                                       required></input>
 
-                                    <div id="buttonWrapper">
-                                        <button id="switchBtn" type="button" onClick={switchPageNum}>Back</button>
-                                        <button id="actionBtn" type="button" >Sign Up</button>
-                                    </div>
-                                </>
-                            )
-                        }
+                                <div id="buttonWrapper">
+                                    <button id="switchBtn" type="button" onClick={switchPageNum}>Back</button>
+                                    <button id="actionBtn" type="button">Sign Up</button>
+                                </div>
+                            </>
+                        )
+                    }
+                    <GoogleButton/>
+                    <strong id="loginResponse">{response}</strong>
+                </form>
 
-                        <strong id="loginResponse">{response}</strong>
-                    </form>
-                </div>
+            </div>
+
         </div>
     );
 }
