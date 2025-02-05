fa8ccedd0a8aa4561d65ab0bbfef6b73b8243d50
psylc12 <psylc12@nottingham.ac.uk>
2025-02-05 15:54:05 +0000

    add checks for present firstName input

--- src/frontend/src/pages/SignUp.tsx
+++ src/frontend/src/pages/SignUp.tsx
@@ -4,7 +4,6 @@ import Header from '../components/Header'
 import "../controller/Authentication";
 import {useNavigate} from 'react-router-dom';
 
-
 /**
  * Creates the elements for the SignUp page
  *@returns HTML elements of the SignUp page
@@ -13,20 +12,29 @@ function SignUp () {
     const navigation = useNavigate();
     const [pageNum, setNum] = useState(1)
     const formReference = useRef<HTMLFormElement>(null);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [response, setResponse] = useState('');
 
     /**
      * Flips the signUp page number to update the page attributes
      */
     function switchPageNum(){
-        if (formReference.current){
-            formReference.current.reset();
+        if(firstName.trim() === "") {
+            setResponse("First Name Required");
+            return;
         }
 
         if (pageNum === 1) {
             setNum(2)
+            setResponse("");
         }else{
             setNum(1);
         }
+
+
     }
 
 
@@ -40,10 +48,14 @@ function SignUp () {
                             pageNum === 1? (
                                 <>
                                     <label>First Name</label>
-                                    <input type="text" required></input>
+                                    <input type="text" id = "firstName" value = {firstName}
+                                           onChange={(e) => setFirstName(e.target.value.trim())}
+                                           required></input>
 
                                     <label>Last Name</label>
-                                    <input type="text" required></input>
+                                    <input type="text" id = "lastName" placeholder="(optional)" value = {lastName}
+                                           onChange={(e) => setLastName(e.target.value.trim())}
+                                           required></input>
 
                                     <div id="buttonWrapper">
                                         <button id="switchBtn" type="button" onClick={() => navigation('/')}>Log In</button>
@@ -53,10 +65,15 @@ function SignUp () {
                             ) : (
                                 <>
                                     <label>Username</label>
-                                    <input type="text" required></input>
+                                    <input type="text" value = {userName}
+                                           onChange={(e) => setUserName(e.target.value.trim())}
+                                           required
+                                    ></input>
 
                                     <label>Password</label>
-                                    <input type="password" required></input>
+                                    <input type="password" value = {password}
+                                           onChange={(e) => setPassword(e.target.value.trim())}
+                                           required></input>
 
                                     <div id="buttonWrapper">
                                         <button id="switchBtn" type="button" onClick={switchPageNum}>Back</button>
@@ -66,7 +83,7 @@ function SignUp () {
                             )
                         }
 
-                        <strong id="loginResponse"></strong>
+                        <strong id="loginResponse">{response}</strong>
                     </form>
                 </div>
         </div>
