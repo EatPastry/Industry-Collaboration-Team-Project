34917c5256971f1d4d8f139028e4b98ac2074e2d
psylc12 <psylc12@nottingham.ac.uk>
2025-03-07 00:28:24 +0000

    add initial files

---
+++ src/frontend/src/pages/TransactionHub.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TransactionHub(){
+    return (
+        <div className="navBarWrapper">
+            <p>Transaction hub</p>
+        </div>
+    );
+}
+
+export default TransactionHub;
