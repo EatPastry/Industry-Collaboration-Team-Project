51b97468629aea5059b1caacc81652e74eb214ea
psylc12 <psylc12@nottingham.ac.uk>
2025-03-15 20:43:06 +0000

    add parner Name section to transaction hub

--- src/frontend/src/components/Calendar.tsx
+++ src/frontend/src/components/Calendar.tsx
@@ -4,7 +4,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 
-// Using https://mui.com/x/react-date-pickers/date-calendar/
+// Following https://mui.com/x/react-date-pickers/date-calendar/
 function Calendar() {
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -14,7 +14,7 @@ function Calendar() {
                 maxDate = {dayjs('2025-12-31')}
                 views={['day', 'month']}
                 sx = {{
-                    // boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+                    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0)',
                     borderRadius: '5px',
                 }}
             />
