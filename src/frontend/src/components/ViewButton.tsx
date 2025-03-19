b9618b91e193b3eaf929a435a4adf1747a0cf6d5
Perfecto Lacuesta <psypl4@nottingham.ac.uk>
2025-03-19 10:29:12 +0000

    add view button and fix background bug

---
+++ src/frontend/src/components/ViewButton.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface ViewButtonProps {
+  label?: string;
+  onClick?: () => void;
+}
+
+const ViewButton: React.FC<ViewButtonProps> = ({ label = "View", onClick }) => {
+  return (
+    <button
+      style={{
+        backgroundColor: "#4169E1",
+        color: "white",
+        border: "none",
+        borderRadius: "20px",
+        padding: "8px 20px",
+        fontSize: "1.2rem",
+        fontWeight: "400",
+        cursor: "pointer",
+      }}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default ViewButton;
