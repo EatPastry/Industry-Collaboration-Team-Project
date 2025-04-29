c163655aa4baf334c1bb3d3a7f4af0a34fa92a2a
Joshua Gaynor <psyjg22@exmail.nottingham.ac.uk>
2025-04-29 16:37:22 +0100

    add more comments to Story.tsx

--- src/frontend/src/pages/Story.tsx
+++ src/frontend/src/pages/Story.tsx
@@ -45,12 +45,13 @@ to {
 let playing = true;
 let currStoryTime = 0;
 
+/**
+ * Handles all of the story logic
+ * @returns main webpage which contains stories, share button and the carousel
+ */
 function Story() {
   const elementRef = useRef(null);
   const [currentPage, setCurrentPage] = useState(0);
-  //const [transactionAmount, setTransactionAmount] = useState<string | null>(null);
-  //const [loading, setLoading] = useState(true);
-  //const [firstName, setFirstName] = useState('');
     
     let muted = false;
     let pauseTime = 0;
@@ -64,13 +65,15 @@ function Story() {
     <ComparativeStats />,
     <GPTFacts/>
   ];
-
+    //Setup variables for the management of the stories
     const numReps = pages.length;
+    //Length of each story in ms
     const storyLength = 10000;
 
     const navigation = useNavigate();
     const location = useLocation();
 
+    //Check the user is logged in
   useEffect(() => {
     const hasSession = checkSession(navigation);
     return () => {
@@ -154,7 +157,7 @@ function Story() {
       return protectionError;
     }
 
-    
+    //SVGs for buttons
     let pauseButton = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="6" y="4" width="4" height="16"></rect><rect x="14" y="4" width="4" height="16"></rect></svg>;
     let playButton = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>
     let unmuteButton = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path></svg>
@@ -162,16 +165,12 @@ function Story() {
     runStory();
     
     function runStory() {
-          //setFill(100);
-          
-          
-          
-      
-          //console.log(document.getElementsByClassName("story-bar-element"))
-        }
 
-        //const [pAnimation, setPlay] = useState({animationPlayState: playing ? "running" : "paused"})
-        
+        }
+        /**
+         * Handles the creation and visualisation of progress through stories
+         * @returns an array of JSX elements which make up the story bar
+         */
         function StoryBars() {
 
           return (
@@ -248,12 +247,12 @@ function Story() {
             </>
           );
         }
-
+  //Go to the previous story
   function goLeft() {
     setCurrentPage((prev) => Math.max(prev - 1, 0));
     currStoryTime = 0;
   }
-
+  //Go to the next story
   async function goRight() {
       if (currentPage < numReps - 1) {
         setCurrentPage((prev) => Math.min(prev + 1, numReps - 1));
@@ -277,7 +276,10 @@ function Story() {
   function handleRightClick() {
     goRight();
   }
-
+        /**
+         * Handles pausing of the stories, changing the icon shown by the button
+         * @returns a button element which calls the @function play when clicked
+         */
         function PButton() {
           const [fill, setFill] = useState(pauseButton);
           function play() {
@@ -305,7 +307,7 @@ function Story() {
             </>
           )
         } 
-
+        //Handles the muting (for later implementation)
         function MButton() {
           const [fill, setFill] = useState(unmuteButton);
 
